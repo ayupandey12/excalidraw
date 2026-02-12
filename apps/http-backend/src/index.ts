201b9b405e6d488492104ff1a5fe82c8c7649722
ayupandey12 <ayushpandey9802@gmail.com>
2026-02-12 15:38:56 +0530

    added middleware to room route in httpbackend

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,5 +1,15 @@
 import express from "express"
+import { middleware } from "./middleware";
 
 const app=express();
  
+app.post('/signup',(req,res)=>{
+    
+})
+app.post('/signin',(req,res)=>{
+    
+})
+app.post('/room',middleware,(req,res)=>{
+    
+})
 app.listen(3001)
---
+++ apps/http-backend/src/middleware.ts
@@ -0,0 +1,15 @@
+import { NextFunction, Request, Response } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken"
+export const middleware=  (req:Request,res:Response,next:NextFunction)=>{
+   const token=req.headers["authorization"]||"";
+   const decode= jwt.verify(token,"")
+   if(!decode||!(decode as JwtPayload).userId)
+   {
+      res.json({mess:"unuthorized user"})
+      return;
+   }
+   else
+   { 
+      next()
+   }
+}
