fa75a35cbc2f197aa99277d913faaedefaaec28f
ayupandey12 <ayushpandey9802@gmail.com>
2026-02-12 16:23:45 +0530

    updated wsbackend

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,6 +1,10 @@
 import { WebSocketServer } from "ws";
 const wss=new WebSocketServer({port:8080},()=>{console.log("ws server created")})
-wss.on("connection",(ws)=>{
+wss.on("connection",(ws,request)=>{
+  const url=request.url
+  const query=new URLSearchParams(url?.split("?")[1])
+  const token=query.get("token")
+  
   ws.on("error",console.error);
   ws.on("message",(data)=>{
     ws.send(data)
