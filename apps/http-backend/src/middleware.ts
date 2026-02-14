625365a23f9b814c6c2be10d30045eea2badb648
ayupandey12 <ayushpandey9802@gmail.com>
2026-02-14 11:12:42 +0530

    updated ws backend

--- apps/http-backend/src/middleware.ts
+++ apps/http-backend/src/middleware.ts
@@ -4,13 +4,13 @@ import jwt, { JwtPayload } from "jsonwebtoken"
 export const middleware=  (req:Request,res:Response,next:NextFunction)=>{
    const token=req.headers["authorization"]||"";
    const decode= jwt.verify(token,JWT_SECRET)
-   if(!decode||!(decode as JwtPayload).userId)
+   if(!decode)
    {
-      res.json({mess:"unuthorized user"})
+      res.status(403).json({mess:"unuthorized user"})
       return;
    }
    else
-   { 
+   {  req.userId=(decode as JwtPayload).userId;
       next()
    }
 }
--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,11 +1,18 @@
 import { WebSocketServer } from "ws";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { JWT_SECRET } from "@repo/common-jwtsecret/index";
 const wss=new WebSocketServer({port:8080},()=>{console.log("ws server created")})
 wss.on("connection",(ws,request)=>{
+   ws.on("error",console.error);
   const url=request.url
   const query=new URLSearchParams(url?.split("?")[1])
-  const token=query.get("token")
-  
-  ws.on("error",console.error);
+  const token=query.get("token")||""
+  const decode =jwt.verify(token,JWT_SECRET)
+  if(!decode||!(decode as JwtPayload).userId)
+  {
+    ws.close();
+    return ;
+  }
   ws.on("message",(data)=>{
     ws.send(data)
   })
