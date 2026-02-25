2276f1710ff7f389f2262b0056bda1bd536c88ce
ayupandey12 <ayushpandey9802@gmail.com>
2026-02-25 06:38:04 +0530

    updated web formsubmit action

--- apps/web/app/lib/actions/Formsubmit.ts
+++ apps/web/app/lib/actions/Formsubmit.ts
@@ -3,12 +3,19 @@ import axios from "axios"
 import "dotenv/config"
 export async function Formsubmitaction({ type,name,email,password }: { type: "signin" | "signup",name:string,email:string,password:string }) {
     console.log(`${process.env.HTTP_URL}/${type}`)
-    const user=await axios.post(`${process.env.HTTP_URL}/${type}`,{name,email,password})
+   try {
+     const user=await axios.post(`${process.env.HTTP_URL}/${type}`,{name,email,password})
       const message=user.data.mess;
       const token=user.data.token;
       return {
         mess:message,
         token:token
       }
+   } catch (error) {
+    return{
+      mess:"backend server is down!",
+      token:undefined
+    }
+   }
       
      }
